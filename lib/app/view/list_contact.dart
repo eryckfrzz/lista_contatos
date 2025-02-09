245c85201fc3ebb1a79b996c56dbdb5e4b5afc5b
Kaio <kaioericak@gmail.com>
2025-02-09 11:53:22 -0300

    (feat): criação da lista e personalização.
    criação da lista com dados locais e personalização da mesma com ícones de editar e deletar usuário.

--- lib/app/view/list_contact.dart
+++ lib/app/view/list_contact.dart
@@ -2,23 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:lista_contatos/app/my_app.dart';
 
 class ListContact extends StatelessWidget {
-  const ListContact({super.key});
+  ListContact({super.key});
+
+  final list = [
+    {
+      "nome": "Maria",
+      "telefone": "(85) 4002-8922",
+      "avatar":
+          "https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_1280.png",
+    },
+    {
+      "nome": "Ana",
+      "telefone": "(85) 4002-8922",
+      "avatar":
+          "https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_1280.png",
+    },
+    {
+      "nome": "Alex",
+      "telefone": "(85) 4002-8922",
+      "avatar":
+          "https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_1280.png",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de contatos'),
+        title: Center(
+          child: Text(
+            'Lista de contatos',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.blue,
         actions: [
           IconButton(
             onPressed: () {
               Navigator.pushNamed(context, MyApp.CADASTRO);
             },
-            icon: Icon(Icons.add),
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
           ),
         ],
       ),
-      body: ListView(),
+      body: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          var contact = list[index];
+          var avatar = CircleAvatar(
+            backgroundImage: NetworkImage(
+              contact['avatar']!,
+            ),
+          );
+
+          return ListTile(
+            title: Text(
+              contact['nome']!,
+            ),
+            subtitle: Text(
+              contact['telefone']!,
+            ),
+            leading: avatar,
+            trailing: SizedBox(
+              width: 100,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
